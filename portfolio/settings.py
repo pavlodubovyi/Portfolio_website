f7ca2925303e743c59569d180d4c707acaed39b3
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2025-03-03 19:36:27 +0100

    static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -60,6 +60,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Application definition
