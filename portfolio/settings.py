c31cfcbfa99c430154dfd3a46a0d5f2a1a768e6a
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2025-03-04 11:25:17 +0100

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / os.getenv('SQLITE_DB_NAME', 'db.sqlite3'),
     }
 }
 
